652f38bebfd05491d331e3e2cf406521d6073551
Zlatan Radovanovic <annoyingzlatan@gmail.com>
2020-11-21 17:50:04 +0100

    [map] Show infowindow for each AP marker
    Signed-off-by: Zlatan Radovanovic <annoyingzlatan@gmail.com>

--- web/JS/map.js
+++ web/JS/map.js
@@ -1,6 +1,7 @@
 var map;
 var service;
 var infowindow;
+var infowindowOpened;
 var markersArray = [];
 
 function initMap() {
@@ -18,14 +19,13 @@ function initMap() {
   service = new google.maps.places.PlacesService(map);
   service.findPlaceFromQuery(request, function (result, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-      const marker = createMarker(result[0]); //potentially this could be 'you are here!' marker
+      const marker = createMarker(result[0], false, undefined); //potentially this could be 'you are here!' marker
       const latitude = result[0].geometry.location.lat();
       const longitude = result[0].geometry.location.lng();
       const place = {
         location: new google.maps.LatLng(latitude, longitude),
         radius: 1000,
         type: ["cafe"],
-        //name: 'genelec'
       };
       map.setCenter(result[0].geometry.location);
       var circle = new google.maps.Circle({
@@ -42,49 +42,74 @@ function initMap() {
 function findAccessPoints(place) {
   service.nearbySearch(place, (results) => {
     for (let i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-      //   console.log(results[i]);
+      parseAP(results[i]);
     }
   });
 }
 
-function createMarker(accessPoint) {
+function parseAP(accessPoint) {
+  const name = accessPoint.name;
+  const place_id = accessPoint.place_id;
+  const rating = accessPoint.rating;
+  const address = accessPoint.vicinity;
+  const types = accessPoint.types;
+  let contentString = `<div id="infowindow">`;
+  if (name != undefined) {
+    contentString += `<h2> Name: ${name} </h2>`;
+  }
+  if (rating != undefined) {
+    contentString += `<br> <b>Rating: ${rating}</b>`;
+  }
+  if (address != undefined) {
+    contentString += `<br> <b>Address: ${address}</b>`;
+  }
+  if (types != undefined) {
+    contentString += `<br> <b>Types of interest:</b> `;
+    types.forEach((type) => {
+      contentString += ` <br>-${String(type)} `;
+    });
+  }
+  contentString += "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+  createMarker(accessPoint, true, infowindow);
+}
+
+function createMarker(accessPoint, display, infowindow) {
   const latitude = accessPoint.geometry.location.lat();
   const longitude = accessPoint.geometry.location.lng();
   const title = accessPoint.name;
-  var icon = {
-    url: accessPoint.icon, // url
-    scaledSize: new google.maps.Size(30, 30), // scaled size
-    origin: new google.maps.Point(0, 0), // origin
-    anchor: new google.maps.Point(0, 0), // anchor
-  };
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(latitude, longitude),
-    map: map,
-    title: title,
-    icon: icon,
-  });
-  const request = {
-    placeId: accessPoint.place_id,
-    fields: [
-      "name",
-      "rating",
-      "formatted_phone_number",
-      "opening_hours.weekday_text",
-    ],
-  };
-  service.getDetails(request, (place, status) => {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      try {
-        const name = place.name;
-        const opening_hours = place.opening_hours.weekday_text;
-        const rating = place.rating;
-        const phone_number = place.phone_num;
-      } catch (err) {
-        console.log(err);
+  let marker = undefined; //will be for sure initialized
+  if (display) {
+    var icon = {
+      url: String(accessPoint.icon), // url
+      scaledSize: new google.maps.Size(30, 30), // scaled size
+      origin: new google.maps.Point(0, 0), // origin
+      anchor: new google.maps.Point(0, 0), // anchor
+    };
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(latitude, longitude),
+      map: map,
+      title: title,
+      icon: icon,
+    });
+  } else {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(latitude, longitude),
+      map: map,
+      title: title,
+    });
+  }
+  if (infowindow) {
+    marker.addListener("click", () => {
+      if (infowindowOpened) {
+        infowindowOpened.close();
       }
-    }
-  });
+      infowindow.open(map, marker);
+      infowindowOpened = infowindow;
+    });
+  }
   return marker;
 }
 
